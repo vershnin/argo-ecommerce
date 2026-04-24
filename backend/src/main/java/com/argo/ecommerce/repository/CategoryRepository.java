ca3a069ab420394e616a2ee67cb0e2fcd2d4b156
vershnin <richardorinda@gmail.com>
2026-04-24 23:09:25 +0300

    feat: add CategoryRepository for product category management

---
+++ backend/src/main/java/com/argo/ecommerce/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.argo.ecommerce.repository;
+
+import com.argo.ecommerce.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findBySlug(String slug);
+    boolean existsByName(String name);
+}
