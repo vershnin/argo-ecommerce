400dbd188dcfcac6033dfc5e07f1d58ca4e4de30
vershnin <richardorinda@gmail.com>
2026-04-24 23:09:27 +0300

    feat: add WishlistRepository for user favorite products

---
+++ backend/src/main/java/com/argo/ecommerce/repository/WishlistRepository.java
@@ -0,0 +1,21 @@
+package com.argo.ecommerce.repository;
+
+import com.argo.ecommerce.entity.Wishlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface WishlistRepository extends JpaRepository<Wishlist, Long> {
+
+    @Query("SELECT w FROM Wishlist w JOIN FETCH w.product WHERE w.user.id = :userId")
+    List<Wishlist> findByUserIdWithProduct(@Param("userId") Long userId);
+
+    Optional<Wishlist> findByUserIdAndProductId(Long userId, Long productId);
+
+    boolean existsByUserIdAndProductId(Long userId, Long productId);
+
+    void deleteByUserIdAndProductId(Long userId, Long productId);
+}
