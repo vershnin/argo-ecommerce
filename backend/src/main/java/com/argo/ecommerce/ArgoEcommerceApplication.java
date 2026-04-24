979573f5e92e7a0e54a091e94cbecaf92b64c92a
vershnin <richardorinda@gmail.com>
2026-04-24 23:08:32 +0300

    feat: add Spring Boot main application entry point

---
+++ backend/src/main/java/com/argo/ecommerce/ArgoEcommerceApplication.java
@@ -0,0 +1,11 @@
+package com.argo.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgoEcommerceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ArgoEcommerceApplication.class, args);
+    }
+}
