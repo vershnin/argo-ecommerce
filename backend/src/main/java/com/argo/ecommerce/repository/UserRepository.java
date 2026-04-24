b11a8319f5348abda721e6f5415fca6c481663ca
vershnin <richardorinda@gmail.com>
2026-04-24 23:09:24 +0300

    feat: add UserRepository with email lookup for authentication

---
+++ backend/src/main/java/com/argo/ecommerce/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.argo.ecommerce.repository;
+
+import com.argo.ecommerce.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
