52704f1ed0ae8ef79e4f55264ceaa4573da49ec5
vershnin <richardorinda@gmail.com>
2026-05-29 22:33:51 +0300

    Enable scheduling support in the main application class

--- backend/src/main/java/com/argo/ecommerce/ArgoEcommerceApplication.java
+++ backend/src/main/java/com/argo/ecommerce/ArgoEcommerceApplication.java
@@ -2,8 +2,10 @@ package com.argo.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ArgoEcommerceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ArgoEcommerceApplication.class, args);
