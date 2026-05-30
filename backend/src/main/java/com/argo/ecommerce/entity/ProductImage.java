866a3e62bf721ffa46c83d5090951c44157a1562
vershnin <richardorinda@gmail.com>
2026-05-30 12:11:06 +0300

    Add ProductImage entity and repository for managing product images

---
+++ backend/src/main/java/com/argo/ecommerce/entity/ProductImage.java
@@ -0,0 +1,43 @@
+package com.argo.ecommerce.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product_images")
+public class ProductImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String url;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
---
+++ backend/src/main/java/com/argo/ecommerce/repository/ProductImageRepository.java
@@ -0,0 +1,9 @@
+package com.argo.ecommerce.repository;
+
+import com.argo.ecommerce.entity.ProductImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
+    List<ProductImage> findByProductId(Long productId);
+}
